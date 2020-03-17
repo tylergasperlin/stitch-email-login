4049ce4b97fbeb98741a47899e01bf99df61c51e
Tyler Gasperlin <gasperlint@gmail.com>
2020-03-16 22:17:24 -0700

    Build stitch email auth

--- src/pages/Auth/ConfirmAccount.js
+++ src/pages/Auth/ConfirmAccount.js
@@ -1,23 +1,29 @@
 import React, { Component } from 'react';
-
+import { Stitch, UserPasswordAuthProviderClient } from 'mongodb-stitch-browser-sdk';
 import './ConfirmAccount.css';
 
 class AuthPage extends Component {
-  componentDidMount() {
-    const queryUrl = window.location.search;
-    const queryParams = new URLSearchParams(queryUrl);
-    const token = queryParams.get('token');
-    const tokenId = queryParams.get('tokenId');
-    console.log('Account confirmed');
-    this.props.history.replace('/');
-  }
+    componentDidMount() {
+        const queryUrl = window.location.search;
+        const queryParams = new URLSearchParams(queryUrl);
+        const token = queryParams.get('token');
+        const tokenId = queryParams.get('tokenId');
+        const emailPassClient = Stitch.defaultAppClient.auth.getProviderClient(
+            UserPasswordAuthProviderClient.factory
+        );
+        emailPassClient.confirmUser(token, tokenId).then(
+          this.props.history.replace('/')
+        ).catch(err => {
+          console.log(err)
+        })
+    }
 
-  render() {
-    return (
-      <main className="confirm-account-page">
-        <p>Confirming Account...</p>
-      </main>
-    );
-  }
+    render() {
+        return (
+            <main className='confirm-account-page'>
+                <p>Confirming Account...</p>
+            </main>
+        );
+    }
 }
 export default AuthPage;
